2a157025965bd537ffbe37d6db052e163121bbe2
Epure-Tofanel Carlo <carlo.epure-tofanel@s.unibuc.ro>
2024-06-08 19:51:22 +0300

    modificat un pic comentariile, numele, si adaugat tcp client si server

--- src/tcp_client.py
+++ src/tcp_client.py
@@ -2,25 +2,36 @@
 import socket
 import logging
 import time
-import sys
+import random
+import string
 
-logging.basicConfig(format = u'[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s', level = logging.NOTSET)
+logging.basicConfig(format=u'[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s', level=logging.NOTSET)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=socket.IPPROTO_TCP)
-
 port = 10000
-adresa = 'localhost'
+adresa = '198.7.0.2'  # Replace with the server's IP address
 server_address = (adresa, port)
-mesaj = sys.argv[1]
 
-try:
-    logging.info('Handshake cu %s', str(server_address))
-    sock.connect(server_address)
-    time.sleep(3)
-    sock.send(mesaj.encode('utf-8'))
-    data = sock.recv(1024)
-    logging.info('Content primit: "%s"', data)
+while True:
+    try:
+        logging.info('Connecting to %s', str(server_address))
+        sock.connect(server_address)
 
-finally:
-    logging.info('closing socket')
-    sock.close()
+        while True:
+            # Generate a random message
+            mesaj = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
+            logging.info('Sending message: "%s"', mesaj)
+            sock.send(mesaj.encode('utf-8'))
+
+            data = sock.recv(1024)
+            logging.info('Received: "%s"', data.decode('utf-8'))
+
+            time.sleep(2)  # Delay between sending messages
+
+    except Exception as e:
+        logging.error('Error: %s', str(e))
+        time.sleep(5)  # Wait before reconnecting
+
+    finally:
+        logging.info('Closing socket')
+        sock.close()
--- src/tcp_server.py
+++ src/tcp_server.py
@@ -2,24 +2,43 @@
 import socket
 import logging
 import time
+import random
+import string
 
-logging.basicConfig(format = u'[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s', level = logging.NOTSET)
+logging.basicConfig(format=u'[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s', level=logging.NOTSET)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=socket.IPPROTO_TCP)
-
 port = 10000
-adresa = 'localhost'
+adresa = '0.0.0.0'  # Listen on all available network interfaces
 server_address = (adresa, port)
 sock.bind(server_address)
-logging.info("Serverul a pornit pe %s si portnul portul %d", adresa, port)
+logging.info("Server started on %s and listening on port %d", adresa, port)
 sock.listen(5)
+
 while True:
-    logging.info('Asteptam conexiui...')
+    logging.info('Waiting for connections...')
     conexiune, address = sock.accept()
-    logging.info("Handshake cu %s", address)
-    time.sleep(2)
-    data = conexiune.recv(1024)
-    logging.info('Content primit: "%s"', data)
-    conexiune.send(b"Server a primit mesajul: " + data)
+    logging.info("Connected to %s", address)
+
+    while True:
+        try:
+            data = conexiune.recv(1024)
+            if not data:
+                break
+
+            logging.info('Received: "%s"', data.decode('utf-8'))
+
+            # Generate a random message
+            mesaj = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
+            logging.info('Sending message: "%s"', mesaj)
+            conexiune.send(f"Server received: {data.decode('utf-8')}. Server message: {mesaj}".encode('utf-8'))
+
+            time.sleep(2)  # Delay between sending messages
+
+        except Exception as e:
+            logging.error('Error: %s', str(e))
+            break
+
     conexiune.close()
+
 sock.close()
