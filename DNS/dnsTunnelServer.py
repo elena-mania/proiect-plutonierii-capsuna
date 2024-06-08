b2b8b9908e12d98b92daec2ea7ae1f29e7e07fa3
Epure-Tofanel Carlo <carlo.epure-tofanel@s.unibuc.ro>
2024-06-08 19:30:06 +0300

    Am adaugat dns tunneler script, si client script-ul pt a primi file

---
+++ DNS/dnsTunnelServer.py
@@ -0,0 +1,181 @@
+from scapy.all import *
+from scapy.layers.dns import DNS, DNSRR
+import base64
+import os
+import hashlib
+
+# cream un socket udp care asculta portu 53, daca vrei doar de test fara sa opresti systemd doar pune port 6969 si -p 6969 cand dai dig
+simple_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, proto=socket.IPPROTO_UDP)
+simple_udp.bind(('0.0.0.0', 53))
+
+# domain-urile hardcodate pt DNS basic
+DOMAIN = "calo.ninja"
+SUBDOMAIN = "capsuna.calo.ninja"
+CNAME = "calo.ninja"
+TARGET = "157.230.100.133"
+DOMAIN_TTL = 900
+CNAME_TTL = 300
+
+
+# functie sa citesc si sa dau encode la fila
+def read_file_in_chunks(file_path, chunk_size=255):
+    """
+    citesc fisierul cerut si il impart in chunk uri de 255 octeti
+    fiecare chuk e codificat in base64 si returnat ca un generator(chestie iterabila python
+    chuck size 255 ca atat e maximul
+    """
+    with open(file_path, "rb") as f:
+        while True:
+            chunk = f.read(chunk_size)
+            if not chunk:
+                break
+            yield base64.b64encode(chunk).decode('utf-8')
+
+
+def calculate_md5(file_path):
+    """
+    calculez md5 la fisier
+    """
+    md5_hash = hashlib.md5()
+    with open(file_path, "rb") as file:
+        for chunk in iter(lambda: file.read(4096), b""):
+            md5_hash.update(chunk)
+    return md5_hash.hexdigest()
+
+
+while True:
+    # Asteapta o cerere DNS pe portul 53
+    request, adresa_sursa = simple_udp.recvfrom(65535)
+    # converitm payload-ul in pachet scapy
+    packet = DNS(request)
+    dns = packet.getlayer(DNS)
+    if dns is not None and dns.opcode == 0:  # dns QUERY
+        query = dns.qd.qname.decode('utf-8')
+        print("got for : ", query)
+        print(packet.summary())
+
+        if query == DOMAIN + ".":
+            # raspund cu o intregistrare A pt ala principal
+            a_record = DNSRR(  # DNS Reply pt A record
+                rrname=dns.qd.qname,  # for question
+                ttl=DOMAIN_TTL,  # DNS entry Time to Live
+                type="A",
+                rclass="IN",
+                rdata=TARGET
+            )
+            dns_response = DNS(
+                id=packet[DNS].id,  # DNS replies must have the same ID as requests
+                qr=1,  # 1 for response, 0 for query
+                aa=0,  # Authoritative Answer
+                rd=0,
+                ra=0,
+                rcode=0,  # 0, nicio eroare
+                qd=packet.qd,  # request-ul original
+                an=a_record  # obiectul de reply
+            )
+            print('sending A record response pt:', query, 'cu IP-u: ', TARGET)
+            print(dns_response.summary())
+            simple_udp.sendto(bytes(dns_response), adresa_sursa)
+
+        elif query == SUBDOMAIN + ".":
+            # raspund cu CNAME pt subodmneiu si pt principal sa apara ca pe dig u oficial
+            cname_answer = DNSRR(  # DNS Reply
+                rrname=dns.qd.qname,  # for question
+                ttl=CNAME_TTL,  # DNS entry Time to Live
+                type="CNAME",
+                rclass="IN",
+                rdata=CNAME  # found at CNAME, aparent subdomeniu poate fi ori cname ori A record, dar initial aveam self host deci las cname
+            )
+            a_record = DNSRR(  # DNS Reply pt A record
+                rrname=CNAME + ".",  # for question
+                ttl=DOMAIN_TTL,  # DNS entry Time to Live
+                type="A",
+                rclass="IN",
+                rdata=TARGET
+            )
+            dns_response = DNS(
+                id=packet[DNS].id,  # DNS replies must have the same ID as requests
+                qr=1,  # 1 for response, 0 for query
+                aa=0,  # Authoritative Answer
+                rd=0,
+                ra=0,
+                rcode=0,  # 0, nicio eroare
+                qd=packet.qd,  # request-ul original
+                an=cname_answer  # obiectul de reply
+            )
+            dns_response.an = dns_response.an / a_record  # concatenez A recordu la Cname anwser sa apara ca la dig ambele
+            print('sending cname response pt:', query, 'pointing spre: ', CNAME, ' cu IP-u: ', TARGET)
+            print(dns_response.summary())
+            simple_udp.sendto(bytes(dns_response), adresa_sursa)
+
+        elif query.endswith(".dnstunnel.calo.ninja."):
+            # Extrage indexul fragmentului si numele fisierului din query
+            # extrag indexu chunk ului si numele fisierului din query
+            chunk_index = int(query.split('.')[0]) # mai intai chunk
+            filename = query.split('.')[1] + ".txt" # apoi nume file si adaug ,txt ca asa am in server filele
+            file_path = f"/root/{filename}"  # totul e in ~
+
+            if os.path.isfile(file_path):
+                # citesc fisierul si impart in fragmente
+                chunks = list(read_file_in_chunks(file_path))
+                if chunk_index < len(chunks):
+                    # daca exista, trimite chunk u
+                    chunk = chunks[chunk_index]
+                    txt_record = DNSRR(
+                        rrname=dns.qd.qname,  # for question
+                        ttl=DOMAIN_TTL,  # DNS entry Time to Live
+                        type="TXT",
+                        rclass="IN",
+                        rdata=chunk
+                    )
+                    dns_response = DNS(
+                        id=packet[DNS].id,  # DNS replies must have the same ID as requests
+                        qr=1,  # 1 for response, 0 for query
+                        aa=0,  # Authoritative Answer
+                        rd=0,
+                        ra=0,
+                        rcode=0,  # 0, nicio eroare
+                        qd=packet.qd,  # request-ul original
+                        an=txt_record  # obiectul de reply
+                    )
+                    print(f'sending TXT record chunk {chunk_index} for file: {filename}') # trimit debugging pe server sa stiu unde se afla
+                    simple_udp.sendto(bytes(dns_response), adresa_sursa)
+                else:
+
+                    # calculam md5 checksum u la fila
+                    md5_checksum = calculate_md5(file_path)
+
+
+                    # trimit md5 u ca txt record separat sa fie mai bine definit
+                    txt_record = DNSRR(
+                        rrname=dns.qd.qname,  # for question
+                        ttl=DOMAIN_TTL,  # DNS entry Time to Live
+                        type="TXT",
+                        rclass="IN",
+                        rdata=f"MD5:{md5_checksum}"
+                    )
+                    dns_response = DNS(
+                        id=packet[DNS].id,  # DNS replies must have the same ID as requests
+                        qr=1,  # 1 for response, 0 for query
+                        aa=0,  # Authoritative Answer
+                        rd=0,
+                        ra=0,
+                        rcode=0,  # 0, nicio eroare
+                        qd=packet.qd,  # request-ul original
+                        an=txt_record  # obiectul de reply
+                    )
+                    print(f'Sending MD5 checksum for file: {filename}')
+                    simple_udp.sendto(bytes(dns_response), adresa_sursa)
+            else:
+                # daca n am gasit fila pe server
+                print(f'File not found for query: {query}')
+
+# inchid socket u
+simple_udp.close()
+
+## Inspiratie:
+##  https://medium.com/@darxtrix/tunnel-your-way-to-free-internet-1a2e9120ddc
+##  https://github.com/senisioi/computer-networks/tree/2023/capitolul2
+##  https://dnstunnel.de/#communication
+##  https://github.com/yarrick/iodine
+##  https://dnstunnel.de/
---
+++ DNS/query.py
@@ -0,0 +1,116 @@
+import subprocess
+import base64
+import hashlib
+
+
+
+"""
+SCRIPT PENTRU CLIENT, DOAR PENTRU TRANSFER DE FISIERE DNSTUNNELING, DACA VRETI QUERY-URI NORMALE LA DOMENIU SI SUBDOMENIU FOLOSITI:
+dig @dns.calo.ninja calo.ninja  SAU dig @dns.calo.ninja capsuna.calo.ninja
+"""
+
+
+def query_dns_for_file(domain, subdomain, server):
+    """
+    interoghez sv u DNS pentru a obtine fisierul prin TXT.
+
+    dns_server = '157.230.100.133'  # adresa ip la droplet
+    domain = 'calo.ninja'
+    subdomain = 'fisier.dnstunnel' asta variaza depinzand de ce ceri
+    return = continutul fisierului in base64 si checksumu primit
+    """
+    responses = []
+    chunk_index = 0
+    md5_checksum = None
+    while True:
+        query_domain = f"{chunk_index}.{subdomain}.{domain}"
+        result = (subprocess.run
+            (
+            ['dig', '@' + server, query_domain, 'TXT', '+short'], # execut dig, nu mi a mers libraria pt dns queries direct
+            capture_output=True,
+            text=True
+        ))
+        # extrag record urile TXT din rezultate
+        txt_records = result.stdout.strip().split('\n')
+        txt_records = [record.strip('"') for record in txt_records if record]
+        if not txt_records:
+            break
+        if txt_records[0].startswith("MD5:"):  # asta e mds u la final calculat pe sv
+            md5_checksum = txt_records[0].split(":")[1]
+            break
+        responses.extend(txt_records)
+        chunk_index += 1
+    return responses, md5_checksum
+
+
+def decode_dns_responses(responses):
+    """
+    decodific raspunsurile de la dns din base64 in human speech
+
+    responses = Lista cu raspunsurile DNS (codificate in base64) lista cu base64 u
+    return = texxtul decodificat
+    """
+    base64_string = ''.join(responses)  # concatenez toate chunk urile TXT
+    file_content = base64.b64decode(base64_string)
+    return file_content
+
+
+def calculate_md5(file_path):
+    """
+    calculez MD5 a ce am primit
+
+    file_path = unde e in pc, fisierul luat din dns tunneling
+    return = MD5 u
+    """
+    md5_hash = hashlib.md5() # instanta de obiect md5 hash
+    with open(file_path, "rb") as file: # binary read mode
+        for chunk in iter(lambda: file.read(4096), b""): # 4096 biti o data, mai putin daca nu mai are de unde se opreste la biti empty b""
+            md5_hash.update(chunk)  # calculat incrementat la fiecar echunk
+    return md5_hash.hexdigest()     # returnez ca si hexadecimal string
+
+
+# Domain uri
+dns_server = '157.230.100.133'  # Adresa ip la droplet
+domain = 'calo.ninja'
+subdomain = 'bee.dnstunnel'  # Subdomeniul pt tunneling, inlocuieste aici fisier cu numele oricarui .txt din server
+                                # optiuni: bee  (bee movie script)
+                                # optiuni: largefile  (generat sa fie 1MB random)
+                                # optiuni: fisiermare (50 randuri de bee movie script)
+                                # optiuni: fisier (un rand de text)
+
+# interoghez sv ul si obtin fisierul, si md5 u
+responses, server_md5 = query_dns_for_file(domain, subdomain, dns_server)
+
+# decodific
+file_content = decode_dns_responses(responses)
+
+# schimb ultimul octet cu X in fisierul primit, eroare intentionata sa verific daca chiar merge md5, ca in testare mereu au dat match
+######  file_content = file_content[:-1] + b'X'
+
+# salvez fisierul
+output_file = "received_largefile.txt"
+with open(output_file, "wb") as f:
+    f.write(file_content)
+
+print(f"Fisierul a fost salvat ca {output_file}")
+
+# calculez md5 la textul primit
+received_md5 = calculate_md5(output_file)
+
+# compar md5 u textului primit cu cel trimis de server
+if received_md5 == server_md5:
+    print("Integritatea fisierului a fost verificata. md5-urile se potrivesc")
+    print("MD5 primit:", received_md5)
+    print("MD5 a serverului:", server_md5)
+else:
+    print("Verificarea integritatii fisierului n-a mers ghinion, md5-urile nu se potrivesc")
+    print("MD5 primit:", received_md5)
+    print("MD5 a serverului:", server_md5)
+
+## Inspiratie:
+##  https://medium.com/@darxtrix/tunnel-your-way-to-free-internet-1a2e9120ddc
+##  https://www.tecmint.com/generate-verify-check-files-md5-checksum-linux/
+##  https://github.com/senisioi/computer-networks/tree/2023/capitolul2
+##  https://dnstunnel.de/#communication
+##  https://github.com/yarrick/iodine
+##  https://dnstunnel.de/
